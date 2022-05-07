e0e459f0f2077ed336db41733ca7d11732fe4620
SeolHuiGwan9478 <shg5977@naver.com>
2022-05-07 15:42:12 +0900

    add What is JSX?

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
+// import ReactDOM from 'react-dom/client';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <p id='hello'>안녕 리액트!</p>
+// );
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const item = '맥북';
+function handleClick(){
+  alert('곧 도착합니다!');
+}
+
 root.render(
-  <h1>안녕 리액트!</h1>
+  <>
+    <h1>나만의 {item} 주문하기</h1>
+    <button onClick={handleClick}>확인</button>
+  </>
 );
