b793c6759ba0b7c96d93ec835a689f87913ce8b3
SeolHuiGwan9478 <shg5977@naver.com>
2022-05-07 17:09:41 +0900

    add react element & component

--- src/index.js
+++ src/index.js
@@ -5,16 +5,8 @@
 //   <p id='hello'>안녕 리액트!</p>
 // );
 import ReactDOM from 'react-dom/client';
+import App from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const item = '맥북';
-function handleClick(){
-  alert('곧 도착합니다!');
-}
 
-root.render(
-  <>
-    <h1>나만의 {item} 주문하기</h1>
-    <button onClick={handleClick}>확인</button>
-  </>
-);
+root.render(<App/>);
