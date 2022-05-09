975198a891c42cbaf06b4f453feab50d3e1b5892
SeolHuiGwan9478 <shg5977@naver.com>
2022-05-09 22:41:18 +0900

    add childern prop

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import Dice from './Dice';
+import Button from './Button';
+
 function App(){
     return (
         <div>
+            <div>
+                <Button text="던지기"/>
+                <Button text="처음부터"/>
+            </div>
             <Dice color="red" num={5}/>
         </div>
     );
